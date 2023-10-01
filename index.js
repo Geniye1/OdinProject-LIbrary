0146b84d0c71aa043e270be6ea674e313bead328
Geniye1 <jstrayer57@gmail.com>
2023-10-01 12:23:37 -0500

    Styled and implemented add book popup

--- index.js
+++ index.js
@@ -8,3 +8,8 @@ function addBookToLibrary(book) {
 
 const library = [];
 
+const addBookButton = document.querySelector(".button-container");
+addBookButton.addEventListener("click", () => {
+    var popup = document.querySelector(".add-book-popup");
+    popup.classList.toggle("show");
+});
