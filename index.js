a356464299e6c6ae6c56fae49806e2fe2aa5a5ae
Geniye1 <jstrayer57@gmail.com>
2023-10-02 00:45:26 -0500

    Implemented being able to delete books and update whether or not you have read them, for now this is finished until I get to the storage section

--- index.js
+++ index.js
@@ -18,11 +18,22 @@ const BookBase = {
 
         newBookDiv.addEventListener("click", () => {
             displayBookInformation(this);
-            
+            bookInView = this;
         })
+
+        if (this.hasRead) {
+            hasReadButton.innerHTML = hasReadSvgPath;
+            hasReadButton.classList.add("has-read");
+        }
 
         newBookDiv.appendChild(newBookPTitle);
         bookContainer.appendChild(newBookDiv);
+
+        return this.library.length - 1;
+    },
+
+    deleteBook: function(book) {
+        this.library.splice(this.library.indexOf(book), 1);
     }
 }
 
@@ -35,7 +46,8 @@ function addNewBook(title, author, pageCount, hasRead, review) {
     newBook.hasRead = hasRead;
     newBook.review = review;
 
-    newBook.addToLibrary()
+    // Set ID of book so that if it needs to be deleted it can be referenced via this ID
+    newBook.id = newBook.addToLibrary()
 }
 
 function displayBookInformation(book) {
@@ -50,6 +62,18 @@ function displayBookInformation(book) {
     reviewParagraph.textContent = book.review;
 }
 
+// The current book the user is viewing the information of
+// (used when the user wants to delete it or update the hasRead property of it)
+var bookInView = null;
+
+// SVG PATHS FOR UPDATING THE HASREAD UI OF THE INFORMATION CONTAINER 
+const hasntReadSvgPath = '<path d="M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" />';
+const hasReadSvgPath = '<path d="M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M12,7A5,5 0 0,0 7,12A5,5 0 0,0 12,17A5,5 0 0,0 17,12A5,5 0 0,0 12,7Z" />';
+const hasReadButton = document.querySelector("#has-read-radio-button");
+
+const deleteButton = document.querySelector("#delete-book-button");
+
+// Popups and the current popup to make it easier to generally toggle visibility to whichever popup is currently being called
 const popup = document.querySelector(".add-book-popup");
 const informationPopup = document.querySelector(".information-popup"); 
 var currentPopupShown = null;
@@ -79,11 +103,38 @@ addBookButton.addEventListener("click", () => {
     reviewIn.value = "";
 });
 
+/* INFORMATION OPTIONS BUTTONS */
+// Delete book button
+deleteButton.addEventListener("click", () => {
+    //BookBase.deleteBook(bookInView);
+    // bookInView = null;
+    // currentPopupShown.classList.toggle("show");
+    // Update the HTML first
+    var bookDiv = document.querySelector(`.book[data-book-id="${bookInView.id}"]`);
+    bookDiv.remove();
+    currentPopupShown.classList.toggle("show");
+
+    // Remove the book from the JS library
+    BookBase.deleteBook(bookInView);
+    bookInView = null;
+    console.log(`Library updated, new length: ${BookBase.library.length}`);
+});
+
+// Update hasRead button
+hasReadButton.addEventListener("click", () => {
+    bookInView.updateRead();
+    hasReadButton.innerHTML = hasReadSvgPath;
+    hasReadButton.classList.add("has-read");
+});
+
 // Allow the user to press ESC to close the popup window but ONLY if the popup window is open
 document.addEventListener("keydown", (event) => {
     if (event.key == "Escape" && currentPopupShown != null) {
         currentPopupShown.classList.toggle("show");
         currentPopupShown = null;
+
+        // If it was the information popup, then we have to reset the bookInView variable
+        if (bookInView != null) bookInView = null;
     }
 }, false);
 
